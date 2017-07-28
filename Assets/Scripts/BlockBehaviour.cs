15af14cae87149dcd2987cbdc9a6057b5d44c93b
Jacky2611 <jacky2611@gmail.com>
2017-07-28 16:00:22 +0200

    added BlockBehaviour Script

---
+++ Assets/Scripts/BlockBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehaviour : MonoBehaviour {
+
+    public BoxCollider trigger;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
