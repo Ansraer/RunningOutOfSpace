03ad63e87c4e7896a3b45a12d950bc5d494fc303
d4nin3u <d4nin3u@gmail.com>
2017-07-28 17:00:20 +0200

    Auto stash before merge of "master" and "origin/master"
    moved PlayerController.cs

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    public int playerNumber;
+
+    private string buttonForward = "W";
+    private string buttonRight = "D";
+    private string buttonDown = "S";
+    private string buttonLeft= "A";
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector2 direction = new Vector2();
+
+ 
+        direction.y += 1;
+
+        //change direction
+
+
+
+        this.gameObject.transform.Translate(new Vector3(direction.x, 0, direction.y));
+	}
+}
