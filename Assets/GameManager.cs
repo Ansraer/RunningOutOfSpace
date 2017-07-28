00fc08d3ab107da5b1ebb7bd3d4d0437e8565819
Jacky2611 <jacky2611@gmail.com>
2017-07-28 18:49:30 +0200

    added GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this when the game starts
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame for graphics stuff
+	void Update () {
+		
+	}
+
+    // Update is called once per game tick
+    void FixedUpdate()
+    {
+
+    }
+}
