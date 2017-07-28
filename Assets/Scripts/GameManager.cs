5643a137dc87b469f08d524a30f5942b729385ce
d4nin3u <d4nin3u@gmail.com>
2017-07-28 19:13:20 +0200

    There's a platform now

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public Transform groundCube;
+
 	// Use this when the game starts
 	void Start () {
-		
+        int length = 20;
+        int width = 20;
+        for(int i = 0; i<length; i++)
+        {
+            for(int j = 0; j<width; j++)
+            {
+                Instantiate(groundCube, new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }    	
 	}
 	
 	// Update is called once per frame for graphics stuff
